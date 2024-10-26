4c392499308c860f3a76a780982575ed2b1be996
taarush570 <taarush2705@outlook.com>
2024-10-26 19:09:44 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+#creating a dictionary which is a key vale pair
+#student= {"Name": "Taarush", "Age": 12 , "Country": "UK", "Year": "IFA"}
+#print(student)
+#print (student ['Country'])
+#print (student['Name'])
+#print (student['Age'])
+#print (student['Year'])
+#adding new element to the dictionary
+#student["height"]=155
+#print (student)
+#student["hobbies"]="cricket","art","video games"
+#print (student)
+#student["favourite colour"]="blue"
+#print (student)
+#print (student.keys())
+#print (student.values())
+#find the number of elements/size of the dictionary
+#print (len(student))
+#check if name exists in dictionary
+#print ("hobbies" in student) 
+#print ("favourite food" in student)
+
+#homework
+#basic functions of a list
+list = {"name":"taarush","age":"12","teacher":"ms sanjana"}
+print (list)
+print (list["teacher"])
+print (list)
+print (list)
+print (list.keys())
+print (list.values())
+print (len(list))
+print ("name in student")
+
+
